5f103d900ce3eb86a78b978c30760aba24837c2d
Philip Holst <philipholst1@gmail.com>
2025-02-28 21:11:26 -0600

    Worked in gradle.build files to add firebase support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,4 +71,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.firebase.bom)
+    implementation(libs.google.firebase.auth)
+    implementation(libs.firebase.database)
 }
