3455fe235b6efecb9d0d05a499278a835abce9c0
Tess Boyer <tess.boyer@my.wheaton.edu>
2025-03-04 19:46:37 -0600

    the event application form can add very basic data to database!

---
+++ app/src/main/java/com/csci335/bulletin/EventApplication.java
@@ -0,0 +1,20 @@
+package com.csci335.bulletin;
+
+public class EventApplication {
+    public String date;
+
+    public EventApplication() {
+    }
+
+    public EventApplication(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
