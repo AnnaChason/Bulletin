1aad0ae699961ea52693271cea8bdacbbac38211
Philip Holst <philipholst1@gmail.com>
2025-03-05 08:26:43 -0600

    Commiting changes that conflict with our new version to stash them and get on the same version as others.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
+    //id("com.android.application")
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.gms.google.services)
+    //baseid("com.google.gms.google-services")
 }
 
 android {
