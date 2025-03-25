12218ecaba6d90ec7b09e5677990d183f6d142f0
Philip Holst <philipholst1@gmail.com>
2025-03-24 19:06:20 -0500

    More work on StudentInfoForm xml layout

--- app/src/main/java/com/csci335/bulletin/StudentInfoForm.java
+++ app/src/main/java/com/csci335/bulletin/StudentInfoForm.java
@@ -1,12 +1,17 @@
 package com.csci335.bulletin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.csci335.bulletin.Mockups.OrganizationApproval;
 
 public class StudentInfoForm extends AppCompatActivity {
 
@@ -21,6 +26,15 @@ public class StudentInfoForm extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button confirmationButton = findViewById(R.id.ConfirmationButton);
+        confirmationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homePage = new Intent(getApplicationContext(), HomePage.class);
+                startActivity(homePage);
+            }
         });
     }
 }
