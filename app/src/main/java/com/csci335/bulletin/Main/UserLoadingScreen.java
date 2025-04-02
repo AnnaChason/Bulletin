8096cc44bc82419f87b8d634f084ea8074b518b1
AnnaChason <annachason.wiggins@my.wheaton.edu>
2025-04-01 20:22:54 -0500

    created loading page to process which type of user the current one is then redirect them

---
+++ app/src/main/java/com/csci335/bulletin/Main/UserLoadingScreen.java
@@ -0,0 +1,30 @@
+package com.csci335.bulletin.Main;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.csci335.bulletin.R;
+/*
+this event will just be a loading page
+where we process which type of user the current user is
+then redirect them to the correct page.
+ */
+public class UserLoadingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_user_loading_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
