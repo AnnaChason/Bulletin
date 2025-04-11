8e53368a1220e8a929d0f926b72bc590a588245f
Philip Holst <philipholst1@gmail.com>
2025-04-10 20:06:28 -0500

    Troubleshooting StudentInfoForm. It works now

--- app/src/main/java/com/csci335/bulletin/StudentClasses/Student.java
+++ app/src/main/java/com/csci335/bulletin/StudentClasses/Student.java
@@ -1,6 +1,9 @@
 package com.csci335.bulletin.StudentClasses;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+import java.util.HashSet;
 
 public class Student {
     private String ID;
@@ -10,23 +13,22 @@ public class Student {
     private int age;
     private Year year;
     private Gender gender;
-    private Set<SpecialStatus> specialStatus;
+    private List<SpecialStatus> specialStatus;
 
     // unneeded?
-    public Student(String ID){
+    public Student(String ID, ArrayList<SpecialStatus> status){
         this.ID = ID;
+        this.specialStatus = status;
     }
 
-    public Student() {}
+    public Student() {
+        specialStatus = new ArrayList<SpecialStatus>();
+    }
 
     public String getID() { return ID; }
     public void setID(String ID) { this.ID = ID; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
 
     // should check permissions?
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
     public int getAge() { return age; }
@@ -35,6 +37,6 @@ public class Student {
     public void setYear(Year year) { this.year = year; }
     public Gender getGender() { return gender; }
     public void setGender(Gender gender) { this.gender = gender; }
-    public Set<SpecialStatus> getSpecialStatus() { return specialStatus; }
+    public List<SpecialStatus> getSpecialStatus() { return specialStatus; }
     public void setSpecialStatus(SpecialStatus specialStatus) { this.specialStatus.add(specialStatus); }
 }
