e23e3595fa55f0924b834cc4cdbe6fbfcb2e632a
Tess Boyer <tess.boyer@my.wheaton.edu>
2025-04-21 21:06:05 -0500

    checkbox filtering maybe???

--- app/src/main/java/com/csci335/bulletin/StudentClasses/Search.java
+++ app/src/main/java/com/csci335/bulletin/StudentClasses/Search.java
@@ -5,17 +5,30 @@ import android.view.View;
 import android.widget.CheckBox;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.csci335.bulletin.Events.Event;
+import com.csci335.bulletin.Events.EventRecyclerViewAdapter;
 import com.csci335.bulletin.Main.NavigationManager;
 import com.csci335.bulletin.Main.Profile;
 import com.csci335.bulletin.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class Search extends AppCompatActivity {
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,17 +55,38 @@ public class Search extends AppCompatActivity {
         CheckBox[] boxes = {sportCheckBox, performanceCheckBox, ministryServiceCheckBox,
                 speakerCheckBox, faithCheckBox, movieGamesCheckBox, informationalCheckBox,
                 artCheckBox, foodCheckBox, academicsCheckBox};
+        ArrayList<String> checkedCats = new ArrayList<String>();
+
+        // recycler view
+        RecyclerView searchRV = findViewById(R.id.recyclerView);
+        ArrayList<Event> events = Event.setUpEvents(searchRV);
+        EventRecyclerViewAdapter rvAdapter = new EventRecyclerViewAdapter(this, events, false);
+        searchRV.setAdapter(rvAdapter);
+        searchRV.setLayoutManager(new LinearLayoutManager(this));
 
         View.OnClickListener checkBoxListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CheckBox cb = (CheckBox) v;
+                int pos = cbToPos(cb, boxes);
                 // add to the list of categories if checked
-
+                if (cb.isChecked()) {
+                    checkedCats.add(Event.categoryOptions()[pos]);
+                }
                 // remove from list if unchecked
+                else { checkedCats.remove(Event.categoryOptions()[pos]); }
 
                 // either way, update the recycler view
-                // adapter.notifyDatasetChanged();
+                db.collection("approvedEvents").whereIn("category", checkedCats).get().addOnCompleteListener(
+                        new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    events.add(document.toObject(Event.class));
+                                }
+                                rvAdapter.notifyDataSetChanged();
+                         }
+                });
             }
         };
 
@@ -61,10 +95,24 @@ public class Search extends AppCompatActivity {
         }
          /*
         Bottom Navigation Bar Manager
-         */
+        */
         NavigationBarView btmNavBarMain = findViewById(R.id.btmNavBarS);
         btmNavBarMain.setSelectedItemId(R.id.other);
         new NavigationManager(btmNavBarMain, Search.this);
 
+    }
+
+    /**
+     * cbToPos takes a given checkbox object and finds its integer position in a given array of boxes
+     * @param box the checkbox to search for
+     * @param boxes list of checkboxes that it is in
+     * @return integer representing the cb's position
+     */
+    public static int cbToPos(CheckBox box, CheckBox[] boxes) {
+        int counter = 0;
+        while (!(box.equals(boxes[counter]))) {
+            counter++;
+        }
+        return counter;
     }
 }
